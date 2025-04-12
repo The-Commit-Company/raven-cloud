5797672255da0138db4b944fa3250433258490ff
prathameshkurunkar7 <prathamesh.kurunkar@thecommit.company>
2025-04-12 15:02:36 +0530

    feat(doctype): add RC Invalid Tokens

---
+++ raven_cloud/raven_cloud/doctype/rc_invalid_tokens/rc_invalid_tokens.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, The Commit Company (Algocode Technologies Private Limited) and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("RC Invalid Tokens", {
+// 	refresh(frm) {
+
+// 	},
+// });
