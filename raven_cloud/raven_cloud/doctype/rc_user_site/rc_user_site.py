510a07437db1d446178aad8515f29bb994a3f419
prathameshkurunkar7 <prathamesh.kurunkar@thecommit.company>
2025-04-25 18:41:36 +0530

    feat(doctype): add RC User Site

---
+++ raven_cloud/raven_cloud/doctype/rc_user_site/rc_user_site.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2025, The Commit Company (Algocode Technologies Private Limited) and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class RCUserSite(Document):
+	# begin: auto-generated types
+	# ruff: noqa
+
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		site: DF.Data
+	# ruff: noqa
+	# end: auto-generated types
+
+
+	pass
