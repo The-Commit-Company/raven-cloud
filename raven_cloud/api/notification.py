f2f7d1331f4fd38d515458a14c4f613d32f87929
prathameshkurunkar7 <prathamesh.kurunkar@thecommit.company>
2025-04-25 17:57:48 +0530

    feat(notification): enhance _send function to improve error handling

--- raven_cloud/api/notification.py
+++ raven_cloud/api/notification.py
@@ -56,94 +56,104 @@ def _send(messages, site_url: str):
     fcm_messages = []
     all_tokens = []
 
-    for message in messages:
-        notification = None
-        data = None
-        webpush = None
-        android = None
-        apns = None
+    try:
+        for message in messages:
+            notification = None
+            data = None
+            webpush = None
+            android = None
+            apns = None
 
-        if message.get("notification"):
-            notification = messaging.Notification(
-                title=message["notification"]["title"],
-                body=message["notification"]["body"],
-                image=message.get("image", None),
-            )
-
-            if message.get("click_action"):
-                webpush = messaging.WebpushConfig(
-                    fcm_options=messaging.WebpushFcmOptions(
-                        link=message["notification"]["click_action"],
-                    ),
-                )
-            
-            if message.get("tag") or message.get("image"):
-                android = messaging.AndroidConfig(
-                    notification=messaging.AndroidNotification(
-                        tag=message.get("tag", None),
-                        image=message.get("image", None),
-                        priority="high",
-                    ),
-                )
-            
-            apns = messaging.APNSConfig(
-                fcm_options=messaging.APNSFCMOptions(
+            if message.get("notification"):
+                notification = messaging.Notification(
+                    title=message["notification"]["title"],
+                    body=message["notification"]["body"],
                     image=message.get("image", None),
-                ),
-                payload=messaging.APNSPayload(
-                    aps=messaging.Aps(
-                        content_available=True,
+                )
+
+                if message.get("click_action"):
+                    webpush = messaging.WebpushConfig(
+                        fcm_options=messaging.WebpushFcmOptions(
+                            link=message["notification"]["click_action"],
+                        ),
+                    )
+                
+                if message.get("tag") or message.get("image"):
+                    android = messaging.AndroidConfig(
+                        notification=messaging.AndroidNotification(
+                            tag=message.get("tag", None),
+                            image=message.get("image", None),
+                            priority="high",
+                        ),
+                    )
+                
+                apns = messaging.APNSConfig(
+                    fcm_options=messaging.APNSFCMOptions(
+                        image=message.get("image", None),
                     ),
-                ),
-            )
-            
+                    payload=messaging.APNSPayload(
+                        aps=messaging.Aps(
+                            content_available=True,
+                        ),
+                    ),
+                )
+                
 
-        if message.get("data"):
-            data = message["data"]
+            if message.get("data"):
+                data = message["data"]
 
-        for token in message.get("tokens", []):
+            for token in message.get("tokens", []):
 
-            fcm_message = messaging.Message(
-                token=token,
-                notification=notification,
-                data=data,
-                webpush=webpush,
-                android=android,
-                apns=apns,
-            )
-            fcm_messages.append(fcm_message)
-            all_tokens.append(token)
+                fcm_message = messaging.Message(
+                    token=token,
+                    notification=notification,
+                    data=data,
+                    webpush=webpush,
+                    android=android,
+                    apns=apns,
+                )
+                fcm_messages.append(fcm_message)
+                all_tokens.append(token)
 
-    response = messaging.send_each(fcm_messages, app=app)
+        # send notifications via fcm in a batch
+        response = messaging.send_each(fcm_messages, app=app)
 
-    failed_tokens = []
-    success_tokens = []
+        failed_tokens = []
+        success_tokens = []
 
-    # failed and success tokens is used to keep track of the count of failed and success tokens and to store the failed/invalid tokens in RC Invalid Tokens doctype
-    if response.failure_count > 0:
-        responses = response.responses
-        for idx, response in enumerate(responses):
-            if response.success:
-                success_tokens.append(all_tokens[idx])
-            else:
-                failed_tokens.append(all_tokens[idx])
+        # failed and success tokens is used to keep track of the count of failed and success tokens and to store the failed/invalid tokens in RC Invalid Tokens doctype
+        if response.failure_count > 0:
+            responses = response.responses
+            for idx, response in enumerate(responses):
+                if response.success:
+                    success_tokens.append(all_tokens[idx])
+                else:
+                    failed_tokens.append(all_tokens[idx])
 
-        # store the failed/invalid tokens in RC Invalid Tokens doctype
-        for token in failed_tokens:
-            frappe.get_doc({
-                "doctype": "RC Invalid Tokens",
-                "site_url": site_url,
-                "invalid_token": token,
-            }).insert()
-    else:
-        success_tokens = all_tokens
+            # store the failed/invalid tokens in RC Invalid Tokens doctype
+            for token in failed_tokens:
+                frappe.get_doc({
+                    "doctype": "RC Invalid Tokens",
+                    "site_url": site_url,
+                    "invalid_token": token,
+                }).insert()
+        else:
+            success_tokens = all_tokens
 
-    # store the response in RC Push Notification Log
-    frappe.get_doc({
-        "doctype": "RC Push Notification Log",
-        "user": frappe.session.user,
-        "number_of_messages": len(messages),
-        "number_of_tokens": len(all_tokens),
-        "success_tokens": len(success_tokens),
-        "failed_tokens": len(failed_tokens),
-    }).insert()
+        # store the response in RC Push Notification Log
+        frappe.get_doc({
+            "doctype": "RC Push Notification Log",
+            "user": frappe.session.user,
+            "number_of_messages": len(messages),
+            "number_of_tokens": len(all_tokens),
+            "success_tokens": len(success_tokens),
+            "failed_tokens": len(failed_tokens),
+        }).insert()
+
+    except Exception as e:
+        frappe.get_doc({
+            "doctype": "RC Push Notification Error Log",
+            "user": frappe.session.user,
+            "site_url": site_url if site_url else None,
+            "error_traceback": frappe.get_traceback(e),
+        }).insert()
