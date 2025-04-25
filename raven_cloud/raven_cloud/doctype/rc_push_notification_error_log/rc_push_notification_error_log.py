3bad1dd6691321d0685012754a7fada39d05fcaa
prathameshkurunkar7 <prathamesh.kurunkar@thecommit.company>
2025-04-25 17:57:18 +0530

    feat(doctype): add RC Push Notification Error Log

---
+++ raven_cloud/raven_cloud/doctype/rc_push_notification_error_log/rc_push_notification_error_log.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2025, The Commit Company (Algocode Technologies Private Limited) and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class RCPushNotificationErrorLog(Document):
+	# begin: auto-generated types
+	# ruff: noqa
+
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		error_traceback: DF.LongText | None
+		site_url: DF.Data | None
+		user: DF.Link
+	# ruff: noqa
+	# end: auto-generated types
+
+
+	pass
